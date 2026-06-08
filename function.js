9f98bad4b355e134aada452b15acae0847c0b371
Gyanadewan <gyanadewan1@gmail.com>
2026-06-09 00:42:26 +0600

    practicing templateLiteral

--- function.js
+++ function.js
@@ -2,9 +2,9 @@
 function add (a,b){
  return a+b
 }
-console.log(add(5,5))
+// console.log(add(5,5))
 
 
 //Arrow Function (ES6 Modern)
 const add2 = (a,b) => a+b 
-console.log (add2(5,3))
+// console.log (add2(5,3))
---
+++ templateLiteral.js
@@ -0,0 +1,7 @@
+let name = "Gyana"
+// console.log(`Hello ${name} `)
+
+
+let name2 = "Gyana Ratan Dewan"
+let age = 20;
+console.log(`${name2} is ${age} years old`)
