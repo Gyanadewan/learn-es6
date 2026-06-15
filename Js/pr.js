d7bc9d7c9f42482720d23ea38417a2e8b11b2434
Gyanadewan <gyanadewan1@gmail.com>
2026-06-15 23:57:10 +0600

    dynamic text added

--- Js/pr.js
+++ Js/pr.js
@@ -1,8 +1,14 @@
+ 
+   const texts = [
+      "Hello JavaScript!",
+   ]
  const handleTextButton = () => {
    const container = document.getElementById("container")
    const div = document.createElement("div")
    div.innerHTML = `
-      <h1> hello bondura kemon acho? </h1>
+       <h1>${texts}</h1>
+     
+      
    `
    container.appendChild(div)
  }
