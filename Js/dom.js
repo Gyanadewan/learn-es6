9d23967e1d585ef0dccd0b6f110ccddf73be5c36
Gyanadewan <gyanadewan1@gmail.com>
2026-06-29 21:41:13 +0600

    practice dom btn click or condition

---
+++ Js/dom.js
@@ -0,0 +1,12 @@
+
+
+function handleBtn (){
+    const email = document.getElementById("email").value
+   const pass = document.getElementById("pass").value
+   if(email === "jamal@gmail.com" && pass === "jamalkan"){
+     alert("successfull")
+   }
+ else{
+    alert("wrong")
+ }
+}
