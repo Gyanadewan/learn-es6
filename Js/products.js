9754074db0d4aa019f37ffcdf6c6df20a34f63c3
Gyanadewan <gyanadewan1@gmail.com>
2026-07-12 22:29:08 +0600

    practice map

--- Js/products.js
+++ Js/products.js
@@ -1,8 +1,19 @@
+// const products = [
+//   { name: "Laptop", price: 50000 },
+//   { name: "Phone", price: 30000 },
+//   { name: "Mouse", price: 1000 }
+// ];
+
+// const highPriceProducts = products.find(highPrice => highPrice.price >=30000);
+// // console.log(highPriceProducts)
+
+
 const products = [
-  { name: "Laptop", price: 50000 },
-  { name: "Phone", price: 30000 },
-  { name: "Mouse", price: 1000 }
+  { id: 1, name: "Laptop", price: 50000, category: "Electronics" },
+  { id: 2, name: "Phone", price: 30000, category: "Electronics" },
+  { id: 3, name: "Mouse", price: 1000, category: "Accessories" },
+  { id: 4, name: "Keyboard", price: 2000, category: "Accessories" },
+  { id: 5, name: "Monitor", price: 15000, category: "Electronics" }
 ];
 
-const highPriceProducts = products.find(highPrice => highPrice.price >=30000);
-console.log(highPriceProducts)
+ products.map(p => console.log(p))
