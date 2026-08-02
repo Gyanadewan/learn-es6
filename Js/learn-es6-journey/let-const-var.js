7eb2a9c3f1beeb005204dcfa0877e8c6baacca3c
Gyanadewan <gyanadewan1@gmail.com>
2026-08-02 10:05:39 +0600

    Practice var let const and scope

---
+++ Js/learn-es6-journey/let-const-var.js
@@ -0,0 +1,29 @@
+
+
+var name1 = "Rahul"
+const name2= "Gyana"
+let name3 = "kamal"
+
+console.log(name1,name2, name3)
+
+
+var name ="lalu"
+if (true){
+    var name = "kalu mama"
+    console.log("Inside var:",name)
+}
+
+
+if (true){
+    const name = "kulu"
+    console.log("Inside const :",name)
+}
+
+
+if (true){
+    let name = "balu";
+      console.log("Inside let :",name)
+}
+console.log("outside:",name)
+
+
