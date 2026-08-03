59722dc36ead8e37c68600359a7331d51aa2f639
Gyanadewan <gyanadewan1@gmail.com>
2026-08-03 23:19:48 +0600

    solve problem check var let const declaration

---
+++ Js/learn-es6-journey/optional-chaining.js
@@ -0,0 +1,12 @@
+const students = {
+      name : "Gyana",
+      roll : 20,
+      id : 324566,
+      address :{
+         district : "chittagong",
+         zela : undefined
+      }
+
+}
+
+console.log(students?.address?.zela)
---
+++ problem-solving.js
@@ -0,0 +1,27 @@
+
+const describeDeclaration =(keyword)=>{
+  if (keyword === "var"){
+   return "Can redeclare, can reassign";
+  }
+  else if (keyword === "let"){
+   return "Cannot redeclare,can reassign";
+  }
+  else if (keyword === "const"){
+   return ("Can redeclare,cannot reassign");
+  }
+  else {
+   return "Invalid";
+  }
+}
+
+console.log(describeDeclaration("let"));
+// Cannot redeclare, can reassign
+
+console.log(describeDeclaration("const"));
+// Cannot redeclare, cannot reassign
+
+console.log(describeDeclaration("var"));
+// Can redeclare, can reassign
+
+console.log(describeDeclaration("int"));
+// Invalid
